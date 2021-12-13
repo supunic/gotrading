cf4b7122644598d86873aa9626e4b1c67dbc6dc0
seiya endo <seiya.endo0830@gmail.com>
2021-12-13 18:43:01 +0900

    [update] Ticker api

--- app/main.go
+++ app/main.go
@@ -5,10 +5,16 @@ import (
 	"app/config"
 	"app/utils"
 	"fmt"
+	"time"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-	fmt.Println(apiClient.GetBalance())
+	ticker, _ := apiClient.GetTicker("BTC_JPY")
+
+	fmt.Println(ticker)
+	fmt.Println(ticker.GetMidPrice())
+	fmt.Println(ticker.DateTime())
+	fmt.Println(ticker.TruncateDateTime(time.Hour))
 }
